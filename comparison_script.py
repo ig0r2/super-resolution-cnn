e851d57f73d36a4cdd2090603a4c530eefa6c662
ig0r2 <yo.iggor@gmail.com>
2026-04-01 01:05:45 +0200

    Updated results

--- comparison_script.py
+++ comparison_script.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # CROP_BOX = (1180, 580, 150, 150)  # (x, y, w, h)
     # LR_PATH = 'data/DIV2K/DIV2K_valid_LR_bicubic/X2/0887x2.png'
     # HR_PATH = 'data/DIV2K/DIV2K_valid_HR/0887.png'
-
+    #
     # CROP_BOX = (744, 936, 96, 96)  # (x, y, w, h)
     # LR_PATH = 'data/DIV2K/DIV2K_valid_LR_bicubic/X2/0873x2.png'
     # HR_PATH = 'data/DIV2K/DIV2K_valid_HR/0873.png'
@@ -27,10 +27,6 @@ if __name__ == "__main__":
     # LR_PATH = 'inference/comparison/input/R6.png'
     # HR_PATH = 'inference/comparison/input/R6_gt.png'
 
-    # CROP_BOX = (1010, 730, 96, 96)  # (x, y, w, h)
-    # LR_PATH = 'data/DIV2K/DIV2K_valid_LR_bicubic/X4/0879x4.png'
-    # HR_PATH = 'data/DIV2K/DIV2K_valid_HR/0879.png'
-
     UPSCALE_FACTOR = 2
     METHODS = ['nearest', 'bilinear', 'bicubic', 'lanczos']
     CHECKPOINT_PATHS = [
@@ -40,22 +36,59 @@ if __name__ == "__main__":
         Path("checkpoints/SR_RFDN_2x_4_256.pth"),
         Path("checkpoints/SR_RFDN_2x_4_128.pth"),
         Path("checkpoints/SR_RFDN_2x_1_128.pth"),
-        Path("checkpoints/SR_RFDN_2x_4_52.pth"),
         Path("checkpoints/SR_RFDN_2x_2_48.pth"),
         Path("checkpoints/SR_IMDN_2x_4_52.pth"),
-        Path("checkpoints/SR_CustomERN_2x_b2_w3.pth"),
-        Path("checkpoints/SR_CustomERN_2x_b2_w2.pth"),
-        Path("checkpoints/SR_CustomERN_2x_b2_w1.pth"),
-        Path("checkpoints/SR_CustomERN_2x_b2_w0.5.pth"),
+        Path("checkpoints/SR_FastEDSR_2x_2_8.pth"),
+        Path("checkpoints/SR_FastEDSR_2x_2_16.pth"),
+        Path("checkpoints/SR_FastEDSR_2x_2_64.pth"),
+        Path("checkpoints/SR_FastEDSR_2x_4_64.pth"),
+        Path("checkpoints/SR_FastEDSR_2x_4_128.pth"),
     ]
+
+    # UPSCALE_FACTOR = 3
+    # CROP_BOX = (1000, 720, 192, 192)  # (x, y, w, h)
+    # LR_PATH = 'data/DIV2K/DIV2K_valid_LR_bicubic/X3/0879x3.png'
+    # HR_PATH = 'data/DIV2K/DIV2K_valid_HR/0879.png'
+    #
+    # CHECKPOINT_PATHS = [
+    #     Path("checkpoints/SR_EDSR_3x_6_12.pth"),
+    #     Path("checkpoints/SR_EDSR_3x_4_48.pth"),
+    #     Path("checkpoints/SR_RFDN_3x_2_48.pth"),
+    #     Path("checkpoints/SR_RFDN_3x_1_128.pth"),
+    #     Path("checkpoints/SR_RFDN_3x_2_128.pth"),
+    #     Path("checkpoints/SR_RFDN_3x_6_48.pth"),
+    #     Path("checkpoints/SR_IMDN_3x_6_64.pth"),
+    #     Path("checkpoints/SR_FastEDSR_3x_2_8.pth"),
+    #     Path("checkpoints/SR_FastEDSR_3x_2_16.pth"),
+    #     Path("checkpoints/SR_FastEDSR_3x_2_64.pth"),
+    #     Path("checkpoints/SR_FastEDSR_3x_4_16.pth"),
+    #     Path("checkpoints/SR_FastEDSR_3x_4_64.pth"),
+    #     Path("checkpoints/SR_FastEDSR_3x_4_128.pth"),
+    # ]
+
+    # UPSCALE_FACTOR = 4
+    # CROP_BOX = (1000, 720, 192, 192)  # (x, y, w, h)
+    # LR_PATH = 'data/DIV2K/DIV2K_valid_LR_bicubic/X4/0879x4.png'
+    # HR_PATH = 'data/DIV2K/DIV2K_valid_HR/0879.png'
+
+    # CROP_BOX = (744, 936, 192, 192)  # (x, y, w, h)
+    # LR_PATH = 'data/DIV2K/DIV2K_valid_LR_bicubic/X4/0873x4.png'
+    # HR_PATH = 'data/DIV2K/DIV2K_valid_HR/0873.png'
+
     # CHECKPOINT_PATHS = [
     #     Path("checkpoints/SR_EDSR_4x_6_12.pth"),
-    #     Path("checkpoints/SR_RFDN_4x_6_48.pth"),
+    #     Path("checkpoints/SR_EDSR_4x_4_48.pth"),
+    #     Path("checkpoints/SR_RFDN_4x_2_48.pth"),
     #     Path("checkpoints/SR_RFDN_4x_1_128.pth"),
     #     Path("checkpoints/SR_RFDN_4x_2_128.pth"),
-    #     Path("checkpoints/SR_RFDN_4x_6_52.pth"),
+    #     Path("checkpoints/SR_RFDN_4x_6_48.pth"),
     #     Path("checkpoints/SR_IMDN_4x_6_64.pth"),
-    #     Path("checkpoints/SR_CustomERN_4x_b2_w2.pth"),
+    #     Path("checkpoints/SR_FastEDSR_4x_2_8.pth"),
+    #     Path("checkpoints/SR_FastEDSR_4x_2_16.pth"),
+    #     Path("checkpoints/SR_FastEDSR_4x_2_64.pth"),
+    #     Path("checkpoints/SR_FastEDSR_4x_4_16.pth"),
+    #     Path("checkpoints/SR_FastEDSR_4x_4_64.pth"),
+    #     Path("checkpoints/SR_FastEDSR_4x_4_128.pth"),
     # ]
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
